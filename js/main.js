b2b9d627b7296212c114004e307f09d4a55233f2
Lucas Tavares <lucastavaresffr@gmail.com>
2023-06-29 12:56:08 -0300

    feat: inclusão de formulario

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00', {reverse: true});
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+            endereco: {
+                required: true,
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'E-mail é obrigatório.',
+            telefone: 'Telefone é obrigatório.',
+            cpf: 'CPF é obrigatório.',
+            cep: 'CEP é obrigatório.',
+            endereco: 'Endereço completo é obrigatório.',
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids()
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos!`)
+            }
+        }
+    })
+})
+
+function validaNome(nomeCompleto) {
+    const nomeArray = nomeCompleto.split(' ')
+    return nomeArray.length >= 2
+}
