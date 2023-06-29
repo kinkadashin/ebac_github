d121d735aa7928adad6668629e7ef617569711d6
Lucas Tavares <lucastavaresffr@gmail.com>
2023-06-29 07:11:07 -0300

    docs: alterando documentação

--- main.js
+++ main.js
@@ -1,33 +1,14 @@
-const form = document.getElementById('form');
+$(document).ready(function() {
+    
+    $('form').on('submit', function(e) {
+        e.preventDefault();
 
-function validarNum(n1, n2) {
-    if (n2 > n1) {
-        return true
-    } else {
-        return false
-    }
-}
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-
-    let numeroA = document.getElementById('number1').value;
-    let numeroB = document.getElementById('number2').value;
-    const mensagemSucesso = "Parabéns, tente manter o número B maior, para que o sistema dê certo."
-    const mensagemErro = "Diminua o número A até que o B seja maior"
-    const resultado = validarNum(parseInt(numeroA), parseInt(numeroB))
-    const containerMensagemSucesso = document.querySelector('.message-aprove');
-    const containerMensagemErro = document.querySelector('.message-error')
-
-    if (resultado) {
-        containerMensagemErro.style.display = 'none';
+        const tarefa = $('#nome-tarefa').val();
+        $(`<li class="tarefa">${tarefa}</li>`).appendTo('ul');
+        $('#nome-tarefa').val('')
         
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
-    } else {
-        containerMensagemSucesso.style.display = 'none';
-        
-        containerMensagemErro.innerHTML = mensagemErro;
-        containerMensagemErro.style.display = 'block';
-    }
+        $('li').click(function() {
+            $(this).addClass("finalizada")
+        })
+    })
 })
